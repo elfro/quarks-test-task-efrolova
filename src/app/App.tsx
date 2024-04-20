69c98e1024dcbdb2a02de7727b4b6b1d804a18e2
Ellina Frolova <frolova.ellina@gmail.com>
2024-04-20 22:41:39 +0300

    Add 404 page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 
 import LoginPage from '@/pages/LoginPage/LoginPage';
 import ThreadPage from '@/pages/ThreadPage/ThreadPage';
+import NotFoundPage from '@/pages/NotFoundPage/NotFoundPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path='/' element={<Navigate to='/login' />} />
       <Route path='/login' element={<LoginPage />} />
       <Route path='/thread' element={<ThreadPage />} />
+      <Route path='*' element={<NotFoundPage />} />
     </Routes>
   );
 }
